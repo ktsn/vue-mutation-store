d51fe04240d2ad2a515fe63aa293d0d34bf5fdff
ktsn <ktsn55@gmail.com>
2016-06-09 01:28:40 +0900

    Add cmap

--- src/mutation.ts
+++ src/mutation.ts
@@ -6,6 +6,14 @@ export type AsyncMutation<T> = (val: T) => Promise<Mutation<T>>;
 export const mzero = <T>(val: T) => {};
 export const azero = <T>(val: T) => Promise.resolve(mzero);
 
+export function cmapM<T, U>(t: Mutation<T>, f: (u: U) => T) : Mutation<U> {
+  return u => t(f(u));
+}
+
+export function cmapA<T, U>(t: AsyncMutation<T>, f: (u : U) => T) : AsyncMutation<U> {
+  return u => t(f(u)).then(m => cmapM(m, f));
+}
+
 export function seqM<T>(...ms: Mutation<T>[]) : Mutation<T> {
   return val => ms.forEach(m => m(val));
 }
